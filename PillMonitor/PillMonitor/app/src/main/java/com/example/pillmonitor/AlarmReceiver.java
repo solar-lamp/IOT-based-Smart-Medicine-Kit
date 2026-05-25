594b517cfb45daadc5329bbf42d7654dd9e132eb
Souradeep Dey <129890493+solar-lamp@users.noreply.github.com>
2026-05-25 12:25:55 +0530

    Update comments for clarity in AlarmReceiver

--- PillMonitor/PillMonitor/app/src/main/java/com/example/pillmonitor/AlarmReceiver.java
+++ PillMonitor/PillMonitor/app/src/main/java/com/example/pillmonitor/AlarmReceiver.java
@@ -8,17 +8,16 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context c, Intent i) {
 
-        // 1. Try to grab the custom text (This will only exist if it's a Doctor Appointment)
         String title = i.getStringExtra("title");
         String text = i.getStringExtra("text");
 
         if (title != null && text != null) {
-            // 🌟 DOCTOR APPOINTMENT 🌟
+            // DOCTOR APPOINTMENT
             // Uses the 'false' flag so it plays your gentle/normal sound
             NotificationHelper.show(c, title, text, 8001, false);
 
         } else {
-            // 🌟 PILL REMINDER 🌟
+            // PILL REMINDER 
             // Uses the 'true' flag so it plays your loud/urgent beep!
             NotificationHelper.show(c, "Pill Reminder", "Time to take your medicine", 1001, true);
         }
